c0f8760e4e77fca58252a6b4164b070a21186034
Jacky Li <jacky6042009@hotmail.com>
2021-11-14 02:30:18 -0800

    simplify main.py

--- main.py
+++ main.py
@@ -4,12 +4,7 @@ import requests
 html_text = requests.get("https://www.memoryexpress.com/Category/VideoCards?Search=geforce%20rtx%203060&Sort=Price&PageSize=120").text
 soup = BeautifulSoup(html_text, 'lxml')
 gpuList = soup.find_all('div', class_ = "c-shca-icon-item")
-cardNameList = soup.find_all('div', class_ = "c-shca-icon-item__body-name")
-cardPriceList = soup.find_all('div', class_ = "c-shca-icon-item__summary-list")
-for gpu in cardNameList:
-    cardName = gpu.text.replace("  ", "")
-    cardName = cardName.replace("   ","")
-    for price in cardPriceList:
-        cardPrice = price.text.replace("+", "")
-        cardPrice = cardPrice.replace(" ", "")
-        print(f'{cardName} costs {cardPrice}')
+for gpu in gpuList:
+    cardNameList = gpu.find('div', class_ = "c-shca-icon-item__body-name").text.replace("  ","")
+    cardPriceList = gpu.find('div', class_ = "c-shca-icon-item__summary-list").text.replace("+","")
+    print(f'{cardNameList} costs {cardPriceList}')
